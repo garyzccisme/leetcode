504c7a6847270020e89ce421c80cdf0da5ffa580
garyzcc <33635035+ChichengZhang@users.noreply.github.com>
2020-07-12 22:12:02 -0400

    236

---
+++ solutions/0236.Lowest_Common_Ancestor_of_a_Binary_Tree/python_solution.py
@@ -0,0 +1,76 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+# Straight Forward Solution
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        self.path = []
+        self.is_find = False
+        self.dfs(root, p)
+        p_path = self.path
+
+        self.path = []
+        self.is_find = False
+        self.dfs(root, q)
+        q_path = self.path
+
+        for node_p, node_q in zip(p_path, q_path):
+            if node_p == node_q:
+                ans = node_p
+            else:
+                break
+        return ans
+
+    def dfs(self, node, target):
+        if node and not self.is_find:
+            self.path.append(node)
+            if node == target:
+                self.is_find = True
+                return
+            else:
+                self.dfs(node.left, target)
+                self.dfs(node.right, target)
+                if not self.is_find:
+                    self.path.pop()
+
+
+# Recursive Solution
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
+        self.ans = None
+
+        def recurse_tree(current_node):
+
+            # If reached the end of a branch, return False.
+            if not current_node:
+                return False
+
+            # Left Recursion
+            left = recurse_tree(current_node.left)
+
+            # Right Recursion
+            right = recurse_tree(current_node.right)
+
+            # If the current node is one of p or q
+            mid = current_node == p or current_node == q
+
+            # If any two of the three flags left, right or mid become True.
+            if mid + left + right >= 2:
+                self.ans = current_node
+
+            # Return True if either of the three bool values is True.
+            return mid or left or right
+
+        # Traverse the tree
+        recurse_tree(root)
+        return self.ans
+
+
+
+
